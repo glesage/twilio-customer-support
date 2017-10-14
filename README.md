2ca24ecc651269797cd71ff18da73a90ef36b700
Geoffroy Lesage <gefthefrench@gmail.com>
2017-10-14 05:10:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+twilio-customer-support
---
+++ server.js
@@ -0,0 +1,52 @@
+/* Dependencies */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const sms = require('./lib/sms.js');
+const intercom = require('./lib/intercom.js');
+
+const app = express();
+app.use(bodyParser());
+
+function respond200(req, res, next)
+{
+    res.sendStatus(200);
+}
+
+function respondTwilio(req, res, next)
+{
+    res.status(200).send('<Response></Response>');
+}
+
+app.post('/sms', function (req, res)
+{
+    sms.receive(req.body).then(function (res)
+    {
+        if (res) return intercom.send(res);
+    }).catch(function (err)
+    {
+        console.error('SMS error: ' + JSON.stringify(err));
+    });
+
+    respondTwilio(req, res);
+});
+
+app.post('/intercom', function (req, res)
+{
+    intercom.receive(req.body).then(function (res)
+    {
+        if (res) return sms.send(res);
+    }).catch(function (err)
+    {
+        console.error('Intercom error: ' + JSON.stringify(err));
+    });
+
+    respond200(req, res);
+});
+
+app.use('/', respond200);
+
+app.listen(80, () =>
+{
+    console.info(`Listening on port 80`);
+});
